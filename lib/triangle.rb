5addae7d96754c7036b2a17d230fcb6c1f65e770
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-09 19:13:50 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :length_side_one, :length_side_two, :length_side_three
+  
+  def initialize(length_side_one, length_side_two, length_side_three)
+    @length_side_one = length_side_one
+    @length_side_two = length_side_two
+    @length_side_three = length_side_three
+  end
+  
+  def kind 
+    legal_triangle?
+    if length_side_one == length_side_two && length_side_two == length_side_three
+        :equilateral
+    elsif length_side_one == length_side_two || length_side_two == length_side_three || length_side_one == length_side_three
+        :isosceles
+      else
+        :scalene
+    end
+  end
+  
+  def legal_triangle?
+    legal_triangle = [(length_side_one + length_side_two > length_side_three), (length_side_one + length_side_three > length_side_two), (length_side_two + length_side_three > length_side_one)]
+    [length_side_one, length_side_two, length_side_three].each do |side|
+      legal_triangle << false if side <= 0 
+    raise TriangleError if legal_triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+  
+  
 end
